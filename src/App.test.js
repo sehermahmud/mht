ece137fa92c779dd4918c7d5486f16b687a439fe
sehermahmud <seher.mahmud@gmail.com>
2021-02-27 21:12:37 +0600

    updated

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,15 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+{
+  /*
+    "@date-io/date-fns": "^1.3.13",
+    "@material-ui/core": "^4.11.0",
+    "@material-ui/icons": "^4.9.1",
+    "@material-ui/pickers": "^3.2.10",
+    "axios": "^0.21.0",
+    "date-fns": "^2.0.0-beta.5",
+    "react-export-excel": "^0.5.3",
+*/
+}
